14f54f98ea4d8a71df2f4e257e5987ddae430d09
Woosung Lim <wml5207@psu.edu>
2020-09-03 18:17:49 +0000

    celsius and fahrenheit opposite

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ temp = float(temp)
 unit = input("Enter unit in F/f or C/c: ")
 if unit == "F" or unit == "f":
   Celsius = float(temp)
-  Celsius = (temp * 9/5) + 32
+  Celsius = (temp - 32) * 5/9
   print(f"{str(temp)}° in Fahrenheit is equivalent to {str(Celsius)}° Celcius.")
 elif unit == "C" or unit == "c":
   Fahrenheit = float(temp)
-  Fahrenheit = (temp - 32) * 5/9
+  Fahrenheit = (temp * 9/5) + 32
   print(f"{str(temp)}° in Celsius is equivalent to {str(Fahrenheit)}° Fahrenheit.")
 else:
   print("Invalid unit("+unit+").")
